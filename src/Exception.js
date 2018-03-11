2193ba91c027da9510147612739ea89ca4e6c948
mhio <37205532+mhio@users.noreply.github.com>
2018-03-11 08:11:55 +0000

    jsdoc

--- src/Exception.js
+++ src/Exception.js
@@ -1,8 +1,6 @@
 
 /** 
  * Exception extends the Error class to more easily annotate Javascript errors with metadata.
- * @extends Error
- *
  *
  * ```
  * class MyException extends Exception {}
@@ -12,6 +10,19 @@
  *   code: 14
  * })
  * ``` 
+ *
+ * @extends Error
+ * @param {string} message            - Standard `Error` message
+ * @param {object} metadata           - Extra metadata for the `Exception`
+ * @param {object} metadata.label     - A UI label for the error
+ * @param {object} metadata.simple    - A simple message for humans
+ * @param {object} metadata.code      - An error code
+ * @property {string} name            - Name for the error, defaults to Class.name
+ * @property {string} message         - Standard `Error` message
+ * @property {string} stack           - Get a stack trace where we can
+ * @property {string} label           - A standard place to store a more human readable error messages
+ * @property {string} simple          - Simple error message, for the hoomans
+ * @property {string|number} code     - An error code
  */
 
 class Exception extends Error {
@@ -19,13 +30,9 @@ class Exception extends Error {
   constructor( message, metadata = {} ){
     super(message)
 
-    /** @property {string} name - Standard Exception name */
     this.name = this.constructor.name
-
-    /** @property {string} message - Standard Error message */
     this.message = message
 
-    /** @property {string} metadata.label - Get a stack trace where we can */
     /* istanbul ignore else */
     if (typeof Error.captureStackTrace === 'function'){
       Error.captureStackTrace(this, this.constructor)
@@ -33,13 +40,8 @@ class Exception extends Error {
       this.stack = (new Error(message)).stack
     }
 
-    /** @property {string} metadata.label - A standard place to store a more human readable error messages */
     if ('label' in metadata)   this.label  = metadata.label
-
-    /** @property {string} metadata.simple - Simple error message, for the hoomans */
     if ('simple' in metadata)  this.simple = metadata.simple
-
-    /** @property {string|number} metadata.code - An error code */
     if ('code' in metadata)    this.code   = metadata.code
 
   }
@@ -66,11 +68,14 @@ class Exception extends Error {
 /** 
  * An `Exception` that encapsulate an `Error` 
  * @extends Exception
+ * @property {Error} error         - The original error that is being wrapped
+ * @param {string} message         - Standard `Error` message
+ * @param {object} metadata        - Extra metadata for the object
+ * @param {Error} metadata.error   - `Error` to attach to the `Exception`
 */
 class ErrorException extends Exception {
 
   constructor( message, metadata = {} ){
-    // Make it an `Exception`
     super(message, metadata)
 
     // Required but undefined is fine. 
@@ -91,16 +96,18 @@ class ErrorException extends Exception {
 /** 
  * An `Exception` for the web 
  * @extends Exception
+ * @property {number} status        - HTTP status code
+ * @property {number} statusCode    - Express uses `.statusCode`. See `status`
+ * @param {string} message          - Standard Error message
+ * @param {object} metadata         - Extra metadata for the object
+ * @param {number} metadata.status  - Integer HTTP status code
  */
 class WebException extends Exception {
 
   constructor( message, metadata = {} ){
     super(message, metadata)
-
-    //** A http status code */
     if (this.constructor.status) this.status = this.constructor.status
     if ('status' in metadata)  this.status = metadata.status
-
   }
 
   /**
